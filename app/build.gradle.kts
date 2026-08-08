eac79e76a7594b1862235d0e30c22a8be74bc1c1
amedeo <amedeo.alfonsi@yahoo.com>
2026-08-08 12:04:31 +0200

    Make project build again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "io.github.amedeoalf.dumb_controller"
     compileSdk {
-        version = release(36)
+        version = release(37)
     }
 
     defaultConfig {
